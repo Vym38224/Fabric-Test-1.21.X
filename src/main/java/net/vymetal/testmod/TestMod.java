00e6984a3c34ef6638130fff5d45b0da771f0004
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2025-03-04 18:28:40 +0100

    Add item to the game

--- src/main/java/net/vymetal/testmod/TestMod.java
+++ src/main/java/net/vymetal/testmod/TestMod.java
@@ -2,15 +2,16 @@ package net.vymetal.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.vymetal.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 // important comment
 public class TestMod implements ModInitializer {
-	public static final String MOD_ID = "test-mod";
+	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
