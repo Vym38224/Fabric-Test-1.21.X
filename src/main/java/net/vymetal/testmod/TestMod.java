286f4ebcdfdf02b16fac6420a4d8ad2f802a701e
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2025-03-04 17:48:13 +0100

    add important comment

--- src/main/java/net/vymetal/testmod/TestMod.java
+++ src/main/java/net/vymetal/testmod/TestMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// important comment
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "test-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
