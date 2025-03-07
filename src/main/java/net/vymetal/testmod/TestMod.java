e4b64c81909ecd419a045061a1d45f29fc16a6c4
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2025-03-07 17:32:07 +0100

    Add custom recipes and loot tables

--- src/main/java/net/vymetal/testmod/TestMod.java
+++ src/main/java/net/vymetal/testmod/TestMod.java
@@ -1,17 +1,22 @@
 package net.vymetal.testmod;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.vymetal.testmod.block.ModBlocks;
+import net.vymetal.testmod.item.ModItemGroups;
 import net.vymetal.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-// important comment
+
+
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
