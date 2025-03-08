75adae79374934c90af5917cf6595448fc10d396
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2025-03-08 09:27:11 +0100

    Add advanced item + recipe for it

--- src/main/java/net/vymetal/testmod/item/ModItems.java
+++ src/main/java/net/vymetal/testmod/item/ModItems.java
@@ -7,10 +7,13 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.vymetal.testmod.TestMod;
+import net.vymetal.testmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
 
     private static Item registerItem(String name, Item item){
@@ -24,6 +27,7 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+            entries.add(CHISEL);
         });
     }
 }
